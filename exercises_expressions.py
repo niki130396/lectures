c45ba23dc43b68060c237665869c28d1f54a7a2f
nikolay <niki130396@gmail.com>
2023-10-08 11:57:07 +0300

    Add exercises

--- exercises_expressions.py
+++ exercises_expressions.py
@@ -1,4 +1,22 @@
 
 
-#   1. Create a new variable (using the assignment operator).
+# 1. Create a new variable (using the assignment operator).
 
+
+# 2. Create a statement using the following expression: a + 5 - b (you need to define a and b).
+
+
+# 3. What is the difference between the `/` operator and the `//` operator?
+
+
+# 4. Consider the following statements:
+# x = 10.0
+# y = (x < 100.0) and isinstance(x, float)
+# What is the value of y?
+
+# 5. Write an expression (or statement) to find the square root of 9.
+
+
+# 6. Write a statement to:
+# Create a variable x with the value 100
+# Increase the value of x fivefold using an augmented assignment operator
