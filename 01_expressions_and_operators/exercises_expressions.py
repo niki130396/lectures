27549c1c5b2b2ee953eb16f5dbfe8888077e84e7
ivoivanchev <21@de005808A00>
2023-10-18 22:08:33 +0300

    All exercises are done

--- 01_expressions_and_operators/exercises_expressions.py
+++ 01_expressions_and_operators/exercises_expressions.py
@@ -1,5 +1,4 @@
 
-
 # 1. Create a new variable (using the assignment operator).
 
 new_string = "test"
@@ -23,26 +22,44 @@ print(f"#3.1: {b/a}")  # used for integer division
 print(f"#3.2: {a//b}")  # used for floor division
 
 # 4. Consider the following statements:
-# x = 10.0
-# y = (x < 100.0) and isinstance(x, float)
+x = 10.0
+y = (x < 100.0) and isinstance(x, float)
 # What is the value of y?
+
+if y == (x < 100.0) and isinstance(x, float):
+    print(f"#4: {y}")
+else:
+    print("check")
 
 # 5. Write an expression (or statement) to find the square root of 9.
 
+print(f"#5: {9**0.5}")
 
 # 6. Write a statement to:
 # Create a variable x with the value 100
-# Increase the value of x fivefold using an augmented assignment operator
+x = 100
 
+# Increase the value of x fivefold using an augmented assignment operator
+x *= 5
+print(f"#6: {x}")
 
 # 7. Cast the string defined below into upper case.
 
 s = "this should be upper case"
+s = s.upper()
+print(f"#7: {s}")
 
 # 8. Make sure the below string starts with a capital letter.
 
 x = "this should start with a capital letter."
+x = x.capitalize()
+
+print(f"#8: {x}")
 
 # 9. Swap the cases of the letters of the string below - upper case letters become lower case and vice versa.
 
 z = "tHIS STRING NEEDS IT'S CASE SWAPPED."
+
+z = z.swapcase()
+
+print(f"#9: {z}")
