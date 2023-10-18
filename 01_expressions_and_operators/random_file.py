04b0fd964417f5a8e856365981abd9c43c024c5b
Nikolay <46595821+niki130396@users.noreply.github.com>
2023-10-18 19:07:16 +0300

    Create random_file.py

---
+++ 01_expressions_and_operators/random_file.py
@@ -0,0 +1,33 @@
+# 1. Create a new variable (using the assignment operator).
+
+a = "a"
+# 2. Create a statement using the following expression: a + 5 - b (you need to define a and b).
+
+
+# 3. What is the difference between the `/` operator and the `//` operator?
+
+
+# 4. Consider the following statements:
+# x = 10.0
+# y = (x < 100.0) and isinstance(x, float)
+# What is the value of y?
+
+# 5. Write an expression (or statement) to find the square root of 9.
+
+
+# 6. Write a statement to:
+# Create a variable x with the value 100
+# Increase the value of x fivefold using an augmented assignment operator
+
+
+# 7. Cast the string defined below into upper case.
+
+s = "this should be upper case"
+
+# 8. Make sure the below string starts with a capital letter.
+
+x = "this should start with a capital letter."
+
+# 9. Swap the cases of the letters of the string below - upper case letters become lower case and vice versa.
+
+z = "tHIS STRING NEEDS IT'S CASE SWAPPED."
