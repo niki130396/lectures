7eddca073b1f67948e9fcfd46e9469b5f9b79ea7
DeboraLzrv <148342187+DeboraLzrv@users.noreply.github.com>
2023-10-23 02:07:42 +0300

    Create Exercises_Debora_1.py

---
+++ 01_expressions_and_operators/Exercises_Debora_1.py
@@ -0,0 +1,85 @@
+# 1. Create a new variable (using the assignment operator).
+
+# myname = 'Debora'
+#
+# print(myname)
+
+# 2. Create a statement using the following expression: a + 5 - b (you need to define a and b).
+
+# a = 10
+# b = 20
+#
+# my_exp = a + 5 - b
+#
+# print(my_exp)
+
+# 3. What is the difference between the `/` operator and the `//` operator?
+
+#`/` operator - This division is used for floating-point number. Example:
+
+# a = 20
+# b = 10
+#
+# my_exp_1 = a / b
+#
+# print(my_exp_1)
+
+#`//` operator - This division is used for integer number. Example:
+
+# a = 20
+# b = 10
+#
+# my_exp_2 = a // b
+#
+# print(my_exp_2)
+
+# 4. Consider the following statements:
+# x = 10.0
+# y = (x < 100.0) and isinstance(x, float)
+# What is the value of y?
+
+# x = 10.0
+# y = (x < 100.0) and isinstance(x, float)
+# print (y)
+
+# 5. Write an expression (or statement) to find the square root of 9.
+
+# import math  # Import the math module
+# expression = math.sqrt(9)
+# print (expression)
+
+
+# 6. Write a statement to:
+# Create a variable x with the value 100
+# Increase the value of x fivefold using an augmented assignment operator
+
+# x=100
+# x*=5
+# print(x)
+
+# 7. Cast the string defined below into upper case.
+#
+# s = "this should be upper case"
+#
+# cs = s.upper()
+#
+# print(cs)
+
+
+
+# 8. Make sure the below string starts with a capital letter.
+#
+# x = "this should start with a capital letter."
+#
+# cx = x.capitalize()
+#
+# print(cx)
+
+
+# 9. Swap the cases of the letters of the string below - upper case letters become lower case and vice versa.
+#
+# z = "tHIS STRING NEEDS IT'S CASE SWAPPED."
+#
+# sz = z.swapcase()
+#
+# print(sz)
